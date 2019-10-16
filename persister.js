2086ac779d66d2a8d7db356bb2534320121df839
mssaairaam <46704782+mssaairaam@users.noreply.github.com>
2019-10-16 21:20:30 +0530

    Files to work.

---
+++ persister.js
@@ -0,0 +1,41 @@
+
+const copyFrom = require('pg-copy-streams').from;
+const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+path.basename(path.resolve(process.cwd()));
+const datasourcesConfigFilePath = path.join('database_configuration.json');
+const datasources = JSON.parse(fs.readFileSync(datasourcesConfigFilePath, 'utf8'));
+
+const pool = new Pool({
+    user: datasources.user,
+    host: datasources.host,
+    database: datasources.database,
+    password: datasources.password,
+    port: datasources.port,
+});
+
+ const bulkInsert = () => {
+    pool.connect().then(client=>{
+      let done = () => {
+        client.release();
+        console.log("Bulk Insert Task End.");
+      }
+      var stream = client.query(copyFrom('COPY PERST_BULK_TABLE FROM STDIN DELIMITER \'|\' '));
+      
+      var fileStream = fs.createReadStream('input_data.txt');
+      let onError = strErr => {
+        console.error('Something went wrong:', strErr);
+        done();
+      };
+      fileStream.on('error', onError);
+
+      stream.on('error', onError);
+      stream.on('end',done);
+
+      fileStream.pipe(stream);
+    });
+  }
+
+  // main function - call to do bulk insert
+  bulkInsert();
