57219901c1b997f802d047df4e41d8a13e269fa7
mssaairaam <46704782+mssaairaam@users.noreply.github.com>
2019-10-17 08:58:30 +0530

    Added performance time calculations.

--- persister.js
+++ persister.js
@@ -1,4 +1,5 @@
 
+const { performance } = require('perf_hooks');
 const copyFrom = require('pg-copy-streams').from;
 const { Pool } = require('pg');
 const fs = require('fs');
@@ -6,6 +7,7 @@ const path = require('path');
 path.basename(path.resolve(process.cwd()));
 const datasourcesConfigFilePath = path.join('database_configuration.json');
 const datasources = JSON.parse(fs.readFileSync(datasourcesConfigFilePath, 'utf8'));
+
 
 const pool = new Pool({
     user: datasources.user,
@@ -15,27 +17,36 @@ const pool = new Pool({
     port: datasources.port,
 });
 
- const bulkInsert = () => {
-    pool.connect().then(client=>{
-      let done = () => {
-        client.release();
-        console.log("Bulk Insert Task End.");
-      }
-      var stream = client.query(copyFrom('COPY PERST_BULK_TABLE FROM STDIN DELIMITER \'|\' '));
-      
-      var fileStream = fs.createReadStream('input_data.txt');
-      let onError = strErr => {
-        console.error('Something went wrong:', strErr);
-        done();
-      };
-      fileStream.on('error', onError);
+const bulkInsert = () => {
+    pool.connect().then(client => {
+        var startTime = performance.now();
 
-      stream.on('error', onError);
-      stream.on('end',done);
+        let done = () => {
+            client.release();
+            console.log("Bulk Insert Task End.");
 
-      fileStream.pipe(stream);
+            var endTime = performance.now();
+    
+            console.log("Time taken to process is " + (endTime - startTime) + " milliseconds.");
+        }
+        var stream = client.query(copyFrom('COPY PERST_BULK_TABLE FROM STDIN DELIMITER \'|\' '));
+
+        var fileStream = fs.createReadStream('input_data.txt');
+        let onError = strErr => {
+            console.error('Something went wrong:', strErr);
+            done();
+        };
+        fileStream.on('error', onError);
+
+        stream.on('error', onError);
+        stream.on('end', done);
+        
+        // do bulk insert
+        fileStream.pipe(stream);
     });
-  }
+}
 
-  // main function - call to do bulk insert
-  bulkInsert();
+
+// main function - call to do bulk insert
+bulkInsert();
+
